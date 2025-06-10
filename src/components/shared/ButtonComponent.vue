278e38089e040abcbd23ad97bd5a17cf8fcb2107
Kender <kendermendoza591@gmail.com>
2025-06-10 20:34:23 +0200

    - Added vuetify configuration

---
+++ src/components/shared/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  content: {
+    type: String,
+    required: true
+  }
+});
+
+const emit = defineEmits <{
+  (event: 'onClick'): void
+}> ();
+
+const onClick = () => {
+  emit('onClick')
+}
+</script>
+
+<template>
+  <v-btn color="primary" elevation="4" large @click="onClick">
+    {{ content }}
+  </v-btn>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import { createVuetify } from 'vuetify'
+import { VBtn } from 'vuetify/components'
+import { Ripple } from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components: {
+    VBtn
+  },
+  directives: {
+    Ripple
+  }
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
